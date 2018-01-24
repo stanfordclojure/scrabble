0a02d1bda72739770c5eb071cf7dc9f1c2dc7fe8
Allan Jiang <origami789@gmail.com>
2018-01-23 18:05:50 -0800

    wip: live coding in class

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject scrabble "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
+  :description "Live Coding Scrabble in class"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
--- src/scrabble/core.cljs
+++ src/scrabble/core.cljs
@@ -26,6 +26,87 @@
 
 (def bag (unroll-frequencies const/tile-frequencies))
 
+
+
+(defn make-player
+  "Takes a player name, and outputs player map"
+  [n]
+  {:rack []
+   :name n
+   :score 0})
+
+
+(def state
+  (atom {:board board
+         :bag bag
+         :players [(make-player "Allan")
+                   (make-player "Adi")
+                   (make-player "Rich")]
+         :turn 0}))
+
+
+(defn- draw-tiles
+  "draws at most n tiles from bag
+  returns tiles drawn and remaining tiles"
+  [bag n]
+  {:pre [(coll? bag) (pos-int? n)]}
+  [(take n bag) (drop n bag)])
+
+
+(defn- replenish-rack [bag {:keys [rack] :as player}]
+  (let [rack-size (count rack)
+        [tiles-drawn remaining-bag] (draw-tiles (shuffle bag) (- 7 rack-size))]
+    {:bag remaining-bag
+     :player (update player :rack concat tiles-drawn)}))
+
+
+(defn init-game [{:keys [bag players] :as state}]
+  (let [player-cnt (count players)]
+    (loop [bag bag
+           players players
+           state state]
+      (if (pos? (count players))
+        (let [data (replenish-rack bag (first players))
+              new-bag (:bag data)
+              new-player (:player data)
+              new-state (-> state
+                          (update :players conj new-player)
+                          (update :players subvec 1)
+                          (assoc :bag new-bag))]
+          (recur new-bag (rest players) new-state))
+        state))))
+
+
+(defn- play-tiles
+  "Places tiles, does not perform validation"
+  [board {:keys [tiles] :as player} tile->position]
+  #_(let [new-tiles ]
+    {:board new-board
+     :player new-player}))
+
+
+(defn player-turn [{:keys [bag players turn board] :as state}]
+  )
+
+
+(comment
+  (init-game @state)
+  (replenish-rack [:A :B :C :F :Q :blank :R :A :A :B] (make-player "Adi"))
+  (replenish-rack [:A :B :A :A :B] (make-player "Adi"))
+  (draw-tiles [:A :B :E] 7)
+  (= (make-player "Allan")
+     {:rack [], :name "Allan", :score 0})
+  )
+
+
+(defn main []
+  ;; conditionally start the app based on whether the #main-app-area
+  ;; node is on the page
+  (if-let [node (.getElementById js/document "main-app-area")]
+    (reagent/render node [:div "This is working"])))
+
+(main)
+
 (defcard-rg board
   [view/board {:rows 15 :cols 15}
    (apply concat (-> board
@@ -46,16 +127,5 @@
   [view/tile :A])
 
 
-(defcard-rg tiles
+(defcard-rg rack
   [view/rack (take 7 (shuffle bag))])
-
-
-
-(defn main []
-  ;; conditionally start the app based on whether the #main-app-area
-  ;; node is on the page
-  (if-let [node (.getElementById js/document "main-app-area")]
-    (reagent/render node [:div "This is working"])))
-
-(main)
-
